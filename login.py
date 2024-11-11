aac1d835d9493746f25650f74ba97b090cb0a169
Pavan Sonawane <158765642+Pavan-1410@users.noreply.github.com>
2024-11-11 14:52:58 +0530

    login.py

--- login.py
+++ login.py
@@ -12,25 +12,29 @@ def login():
     password = entry_password.get()
     
     messagebox.showinfo("Login", "Login Successful")
-   
+    entry_username.delete(0,tk.END)
+    entry_password.delete(0,tk.END)
+# Name (title)
+title = tk.Label(root, text="Login Page", font=20, bg="red")
+title.grid(row=0, column=0, columnspan=2, pady=10)  # title spans across two columns
 
 # Username label and entry
 label_username = tk.Label(root, text="Username:")
-label_username.pack(pady=5)
+label_username.grid(row=1, column=0, pady=5, padx=10, sticky="e")  # 'e' aligns text to the right
 
 entry_username = tk.Entry(root)
-entry_username.pack(pady=5)
+entry_username.grid(row=1, column=1, pady=5, padx=10)
 
 # Password label and entry
 label_password = tk.Label(root, text="Password:")
-label_password.pack(pady=5)
+label_password.grid(row=2, column=0, pady=5, padx=10, sticky="e")  # 'e' aligns text to the right
 
 entry_password = tk.Entry(root, show="*")
-entry_password.pack(pady=5)
+entry_password.grid(row=2, column=1, pady=5, padx=10)
 
 # Login button
 button_login = tk.Button(root, text="Login", command=login)
-button_login.pack(pady=20)
+button_login.grid(row=3, column=0, columnspan=2, pady=20)
 
 # Start the Tkinter event loop
 root.mainloop()
